67f43adf8ad643e264ddbbb697f7572bb0929eb3
Gustavo Souza <134634532+SoulzaGus@users.noreply.github.com>
2023-05-25 15:55:58 -0300

    Add files via upload

---
+++ taskscript.js
@@ -0,0 +1,7 @@
+
+Arquivo JavaScript (taskscript.js):
+```js
+function toggleMenu() {
+	var navbarNav = document.getElementById("navbar-nav");
+	navbarNav.classList.toggle("active"); /* Adiciona ou remove a classe "active" na lista de navegação */
+}
